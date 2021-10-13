1deee58370957f2d76d1874c6f34939d5db0e2c1
Pawan Rampal <pawanrampal@gmail.com>
2021-10-12 23:11:00 -0400

    Improving the CLI and Main Function

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import find_packages, setup
 
-with open('README.MD', 'r') as f:
+with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(
     name='pgbackup',
     version='0.1.0',
-    authod='Pawan Rampal'
+    author='Pawan Rampal',
     author_email='pawanrampal@gmail.com',
     description='A utility for backing up PostgreSQL databases.',
     long_desciption=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/pawanrampal/pgbackup',
-    packages=find_packages('src')
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    entry_points={ 'console_scripts': ['pgbackup=pgbackup.cli:main'] },
 )
