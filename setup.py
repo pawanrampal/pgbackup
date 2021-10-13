e08389d48e0e2b9d270fb2f07bce347803a1c829
Pawan Rampal <pawanrampal@gmail.com>
2021-10-12 23:24:18 -0400

    saving files

--- setup.py
+++ setup.py
@@ -15,5 +15,6 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=['boto3'],
+    python_requires='>=3.6',
     entry_points={ 'console_scripts': ['pgbackup=pgbackup.cli:main'] },
 )
